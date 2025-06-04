6e6611588485872036b812d778e3b7ddf88d2df8
JamesS <47437340+splinesreticulating@users.noreply.github.com>
2025-06-03 20:04:16 -0700

    test: alias env module

---
+++ tests/mocks/env-static-private.ts
@@ -0,0 +1,3 @@
+export const JWT_SECRET = process.env.JWT_SECRET || '';
+export const BASIC_AUTH_USERNAME = process.env.BASIC_AUTH_USERNAME || '';
+export const BASIC_AUTH_PASSWORD = process.env.BASIC_AUTH_PASSWORD || '';
