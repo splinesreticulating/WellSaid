f2e12710f1f6234f4e403fc6f4d724acb33245ea
nbp <splinesreticulating@users.noreply.github.com>
2025-06-06 14:16:30 -0700

    chore: remove unused basic auth username configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ dotenv.config()
 
 export default defineConfig({
     plugins: [sveltekit()],
-    define: {
-        'import.meta.env.VITE_BASIC_AUTH_USERNAME': JSON.stringify(
-            process.env.BASIC_AUTH_USERNAME || '',
-        ),
-    },
     server: {
         host: '0.0.0.0', // Listen on all network interfaces
         port: 5173, // Default Vite port, change if needed
