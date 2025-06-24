e0f54cb93a1e5c2585172bb8ab24053e8929b520
nbp <splinesreticulating@users.noreply.github.com>
2025-06-24 11:05:10 -0700

    ui: fix input field lengths

--- src/lib/components/SettingsForm.svelte
+++ src/lib/components/SettingsForm.svelte
@@ -19,8 +19,19 @@
     {#each formSettings as setting}
         <div class="setting-row">
             <label for={setting.key}>{envKeyToHumanReadable(setting.key)}</label>
-            <input id={setting.key} name={setting.key} type="text" bind:value={setting.value} />
-            <p class="description">{setting.description}</p>
+            {#if setting.key === 'CUSTOM_CONTEXT'}
+                <textarea
+                    id={setting.key}
+                    name={setting.key}
+                    bind:value={setting.value}
+                    rows="4"
+                    class="context-textarea"
+                    placeholder={setting.description}>{setting.value}</textarea
+                >
+            {:else}
+                <input id={setting.key} name={setting.key} type="text" bind:value={setting.value} />
+                <p class="description">{setting.description}</p>
+            {/if}
         </div>
     {/each}
     <button type="submit" formaction="/settings" class="save">Save</button>
@@ -34,12 +45,30 @@
         font-weight: bold;
         display: block;
     }
-    input {
+    input,
+    textarea {
         border: 1px solid var(--light);
         border-radius: var(--border-radius);
         padding: 0.5rem;
-        width: 100%;
         box-sizing: border-box;
+        font-family: inherit;
+        font-size: 1rem;
+    }
+
+    textarea {
+        width: 100%;
+        min-height: 100px;
+        resize: vertical;
+    }
+
+    /* Make specific fields narrower */
+    input[name='PARTNER_PHONE'] {
+        width: 200px; /* For phone numbers */
+    }
+
+    input[name='HISTORY_LOOKBACK_HOURS'] {
+        width: 60px; /* Only needs room for up to 3 digits */
+        text-align: center;
     }
     .description {
         font-size: 0.8rem;
